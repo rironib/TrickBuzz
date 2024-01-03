19e7780dc3e63bc7dce3b22301ef28553902026e
Razibul Islam <dev.ronib@gmail.com>
2024-01-03 13:29:44 +0600

    Fix error in 404 header.php

--- templateParts/404/header.php
+++ templateParts/404/header.php
@@ -8,9 +8,12 @@
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css">
 
     <!-- Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $theme_directory_url ?>/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $theme_directory_url ?>/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $theme_directory_url ?>/icons/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();
+                                                        ?>/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();
+                                                            ?>/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();
+                                                            ?>/icons/favicon-16x16.png">
     <link rel="manifest" href="http://localhost/wp/wp-content/themes/DSM/icons/site.webmanifest">
     <link rel="mask-icon" href="http://localhost/wp/wp-content/themes/DSM/icons/safari-pinned-tab.svg" color="#04aa6d">
     <link rel="shortcut icon" href="http://localhost/wp/wp-content/themes/DSM/icons/favicon.ico">
